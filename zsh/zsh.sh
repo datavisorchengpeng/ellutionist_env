fb17f0cc592ab478c097a18e5f6ff43800f188d4
datavisorchengpeng <71744586+datavisorchengpeng@users.noreply.github.com>
2021-08-16 11:18:39 +0800

    add exception check

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -1,19 +1,42 @@
 echo "Installing 'zsh'..."
 sudo yum install -y zsh
+if [ $? -ne '0' ]
+then
+    echo "Install zsh failed."
+    exit 1
+fi
 
 echo "Changing default shell to 'zsh'..."
 chsh -s /bin/zsh
-
-echo "Current default shell: $SEHLL"
+if [ $? -ne '0' ]
+then
+    echo "Change shell failed."
+    exit 1
+fi
 
 echo "Install 'oh-my-zsh'..."
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+if [ $? -ne '0' ]
+then
+    echo "Install oh-my-zsh failed."
+    exit 1
+fi
 
 echo "Cloning 'zsh-syntax-highlighting'..."
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+if [ $? -ne '0' ]
+then
+    echo "Install 'zsh-syntax-highlighting' failed."
+    exit 1
+fi
 
-echo "Cloning 'zsh-autosuggestions'..."
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# echo "Cloning 'zsh-autosuggestions'..."
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# if [ $? -ne '0' ]
+# then
+#     echo "Install 'zsh-syntax-highlighting' failed."
+#     exit 1
+# fi
 
 echo "Add plugins to '.zshrc'..."
 
